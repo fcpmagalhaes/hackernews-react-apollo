2e8e9455c92fd0eec469edb1c283415077880509
Felipe Magalhaes <fcpmagalhaes@hotmail.com>
2023-08-22 19:12:46 -0300

    Add middleware to authentication token

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
 import App from './components/App';
+import './styles/index.css';
 
 // 1
 import {
@@ -17,9 +19,19 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
